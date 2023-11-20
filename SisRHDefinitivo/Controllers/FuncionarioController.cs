1a8c2a33ec79326f18ca3fda65f540f721d250e4
Patrick Fernando <patrickfernando1234@gmail.com>
2023-11-19 23:55:59 -0300

    Ajuste de rotas (Falta criar a controller de Funcionario, esta apenas retornando a view). E ajuste da tela de login em andamento.

---
+++ SisRHDefinitivo/Controllers/FuncionarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SisRHDefinitivo.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
