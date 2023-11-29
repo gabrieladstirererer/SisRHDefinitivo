a695ffb1bae1733de0ef62b0cfc2e85f3e995408
Gabriel Arruda <gabrielarrudaafonso540@gmail.com>
2023-11-29 07:34:44 -0300

    Ajustes na TableViewModel, Criada o arquivo da PartialView

---
+++ SisRHDefinitivo/ViewsModel/Funcionario/NewViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SisRHDefinitivo.Mvc.Adm.Models;
+using System.Web.Mvc;
+
+namespace SisRHDefinitivo.Mvc.Adm.ViewsModel.Funcionario
+{
+    public class NewViewModel : FuncionarioModel
+    {
+        public Mvc.Adm.Models.FuncionarioModel Funcionario { get; set; }
+        public IEnumerable<SelectListItem> TipoEnderecoList { get; set; }
+    }
+}
