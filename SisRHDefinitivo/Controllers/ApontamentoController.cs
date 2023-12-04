72003568a86c319e3dc696ba86473b556381408c
Felipe Vaz <felipecoelho12@outlook.com.br>
2023-12-03 23:56:08 -0300

    Correções

--- SisRHDefinitivo/Controllers/ApontamentoController.cs
+++ SisRHDefinitivo/Controllers/ApontamentoController.cs
@@ -8,30 +8,32 @@ namespace SisRHDefinitivo.Mvc.Adm.Controllers
 {
     public class ApontamentoController : Controller
     {
-        public IActionResult ConsultarFolhaPonto(string prof, string dia, string mes, string ano)
+        public IActionResult ConsultarFolhaPonto(ConsultarFolhaPontoViewModel vm)
         {
             try
             {
 
-                if (prof == null || (dia == null || mes == null || ano == null))
+                if (vm.matricula == null && (vm.dia == 0 || vm.mes == 0 || vm.ano == 0))
                 {
-                    var vm = new ConsultarFolhaPontoViewModel();
+                   // var vm = new ConsultarFolhaPontoViewModel();
                     var folhapontorepo = new FolhaPontoRepository();
 
-                    ModelState.Clear();
-                    //int prof = -1;
-                    //int dia = -1;
-                    //int mes = -1;
-                    //int ano = -1;   
+                    ModelState.Clear();                             
 
-                    //vm.Ponto = folhapontorepo.ConsultarFolhaPonto(prof, dia, mes, ano);            
+                    var Ldia = DateTime.Now.Day - 1;
+                    var Lmes = Ldia == 0 ? DateTime.Now.Month - 1 : DateTime.Now.Month;
+                    var Lano = Lmes == 0 ? DateTime.Now.Year - 1 : DateTime.Now.Year;
+
+                    vm.dia = Ldia != 0 ? Ldia : DateTime.DaysInMonth(Lano, Lmes);
+                    vm.mes = Lmes;
+                    vm.ano = Lano;
                     vm.Ponto = folhapontorepo.ConsultarFolhaPonto(-1, -1, -1, -1);
 
                     return View(vm);
                 }
                 else
                 {
-                    var vm = new ConsultarFolhaPontoViewModel();
+                   // var vm = new ConsultarFolhaPontoViewModel();
                     var folhapontorepo = new FolhaPontoRepository();
                     ModelState.Clear();
 
@@ -41,16 +43,16 @@ namespace SisRHDefinitivo.Mvc.Adm.Controllers
                     var anoP = -1;
                     var matricula = -1;
 
-                    if (Convert.ToInt32(prof) > 0 && prof != null)
+                    if (Convert.ToInt32(vm.matricula) > 0 && vm.matricula != null)
                     {
-                        matricula = Convert.ToInt32(prof);
+                        matricula = Convert.ToInt32(vm.matricula);
                     }
 
-                    if ((Convert.ToInt32(dia) > 0 && Convert.ToInt32(dia) < 32) && (Convert.ToInt32(mes) > 0 && Convert.ToInt32(mes) < 13) && (Convert.ToInt32(ano) > 1900 && Convert.ToInt32(ano) <= DateTime.Now.Year))
+                    if ((Convert.ToInt32(vm.dia) > 0 && Convert.ToInt32(vm.dia) < 32) && (Convert.ToInt32(vm.mes) > 0 && Convert.ToInt32(vm.mes) < 13) && (Convert.ToInt32(vm.ano) > 1900 && Convert.ToInt32(vm.ano) <= DateTime.Now.Year))
                     {
-                        diaP = Convert.ToInt32(dia);
-                        mesP = Convert.ToInt32(mes);
-                        anoP = Convert.ToInt32(ano);
+                        diaP = Convert.ToInt32(vm.dia);
+                        mesP = Convert.ToInt32(vm.mes);
+                        anoP = Convert.ToInt32(vm.ano);
 
                         vm.Ponto = folhapontorepo.ConsultarFolhaPonto(matricula, diaP, mesP, anoP);
 
